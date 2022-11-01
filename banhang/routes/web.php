25f33e9499cc370d205efbe4ac1eb09bd48f890f
leanhkiet071100 <leanhkiet071100@gmail.com>
2022-11-01 08:44:04 +0700

    upload

--- banhang/routes/web.php
+++ banhang/routes/web.php
@@ -13,10 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+// Trang chủ
 Route::get('/', function () {
-    return view('welcome');
+    return view('trangchu.index');
 });
 
+//sản phẩm
 Route::get('/san-pham', function () {
     return view('sanpham.dssanpham');
 });
@@ -24,4 +27,52 @@ Route::get('/san-pham', function () {
 Route::get('/chi-tiet-san-pham', function () {
     return view('sanpham.chitietsanpham');
 });
+
+//bài viết
+Route::get('/bai-viet', function () {
+    return view('baiviet.dsbaiviet');
+});
+
+Route::get('/chi-tiet-bai-viet', function () {
+    return view('baiviet.chitietbaiviet');
+});
+
+//hóa đơn
+Route::get('/xuat-hoa-don', function () {
+    return view('hoadon.xuathoadon');
+});
+
+//giới thiệu
+Route::get('/gioi-thieu', function () {
+    return view('thongtinshop.gioithieu');
+});
+
+// giỏ hàng
+Route::get('/gio-hang', function () {
+    return view('giohang.giohang');
+});
+
+//middlewware  kiểm tra đăng nhập hay chưa
+Route::middleware('auth')->group(function(){});
+
+//admin
+
+Route::prefix('admin')->group(function(){
+    Route::name('admin.')->group(function(){ 
+        //bài viết
+        Route::get('/bai-viet', function () {
+            return view('admin.baiviet.baiviet-ds');
+        });
+
+        Route::get('/chi-tiet-bai-viet', function () {
+            return view('admin.baiviet.baiviet-chitiet');
+        });
+
+        Route::get('/sua-bai-viet', function () {
+            return view('admin.baiviet.baiviet-sua');
+        });
+    });
+        
+});
+
 
