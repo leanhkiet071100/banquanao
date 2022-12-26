f145efba831ad3e898a99df5268a645b66c0e0e1
leanhkiet071100 <leanhkiet071100@gmail.com>
2022-12-27 00:28:43 +0700

    email

--- banhang/app/Providers/AuthServiceProvider.php
+++ banhang/app/Providers/AuthServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+Use Illuminate\Support\Facades\Auth;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -24,7 +25,7 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
-
-        //
+         $iduser = Auth::user()->id;
+        dd($iduser);
     }
 }
